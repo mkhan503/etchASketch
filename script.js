74985079a06ceefc314430b552a6a9dd0f32b2e8
mkhan503 <mahah.mujeebkhan@gmail.com>
2021-09-01 00:17:22 +0400

    added refresh button, set max limit to 100

--- script.js
+++ script.js
@@ -1,47 +1,44 @@
-
+createGrid();
 
 const restartButton = document.querySelector('#restart');
 restartButton.addEventListener('click',refresh);
 
-function refresh(e){
-    const pixels = document.querySelectorAll('.refreshState');
-    pixels.forEach(pixel => pixel.style.backgroundColor='white');
-    getGridSize();
-}
-
-function getGridSize(){
-    let rows = prompt('Please enter grid size (number of squares per side):');
-    let columns = rows;
-    createGrid(rows,columns);
-    
-}
-
-const pixels = document.querySelectorAll('.hoverState');
-pixels.forEach(cell => cell.addEventListener('mouseover',changeColor));
-
 function changeColor(e){
     this.style.backgroundColor = 'grey';
 }
+ //Delete each item in grid and create another grid by calling createGrid;
+function refresh(e){
+    const pixels = document.querySelectorAll('.hoverState');
+    pixels.forEach(item => item.remove());
+    createGrid();
+}
 
+//Create new div and attach to grid container
 function createNewDiv(){
     const newDiv = document.createElement('div');
-   newDiv.classList.add('square', 'hoverState', 'refreshState');
+   newDiv.classList.add('square', 'hoverState');
    const gridContainer = document.querySelector('#gridContainer');
    gridContainer.appendChild(newDiv);
     
     
 }
+// createGrid by prompting number of rows.
+function createGrid(){
 
-function createGrid(rows,columns){
+    let rows = (prompt('Please enter grid size (number of squares per side):'));
+    if(rows > 100){
+        createGrid();
+    }
+    let columns = rows;    
     const total = rows * columns;
     console.log(total);
     for (let i = 0; i < total; i++){
         createNewDiv();
     }
 
-    //creating a dynamic grid
+    const pixels = document.querySelectorAll('.hoverState');
+    pixels.forEach(cell => cell.addEventListener('mouseover',changeColor));
+    
     const gridContainer = document.querySelector('#gridContainer');
     gridContainer.setAttribute('style', `grid-template-rows: repeat(${rows}, 1fr);  grid-template-columns: repeat(${columns}, 1fr);`);
-
-
 }
